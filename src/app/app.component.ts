7a08b86e919bcb735b8546e564da03c7a33a65c0
Steven Quintana <steve.e.quint@gmail.com>
2022-01-05 19:36:42 -0800

    Update list items to a goals array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,5 @@ import { Component } from '@angular/core'
 export class AppComponent {
   title = 'Angular Practice'
   subTitle = 'My Plan'
+  goals = ['Sign up for a MCS', 'Learn Angular', 'Organize GitHub', '5 Year Plan', 'New job or promotion to SDK/Product']
 }
